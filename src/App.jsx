c3d5d9349c0f14d78e788c152f6d2e34353c8472
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2024-03-10 19:45:59 -0400

    remove unnecessary anchor tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <div className="w-screen h-screen bg-pink-500">
-      <a href="https://www.example.com" target="_blank"></a>
-    </div>
-  );
+  return <div className="w-screen h-screen bg-pink-500"></div>;
 }
 
 export default App;
