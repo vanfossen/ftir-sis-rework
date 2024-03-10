36251ead4a5f53900137f1971070a165b5df113f
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2024-03-10 16:46:44 -0400

    base react project using vite

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen bg-pink-500">
+      <a href="https://www.example.com" target="_blank"></a>
+    </div>
+  );
+}
+
+export default App;
