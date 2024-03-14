4c0d8fd0c9ebe413c9ea67af984dd7c54c36db9e
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2024-03-14 15:05:44 -0400

    add svg to indicate dropdown

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
     <nav className="flex justify-evenly bg-[#0f172a] p-2">
       <div className="flex items-center">
         <img src="/vite.svg" alt="test logo" />
-        <a>
+        <a className="cursor-default">
           <span className="text-red-500">F</span>
           <span className="text-orange-500">T</span>
           <span className="text-yellow-500">I</span>
@@ -21,8 +21,22 @@ function NavBar() {
         {navItems.map((item, i) => (
           <div key={i} className="group relative">
             <ul>
-              <li className="cursor-default p-2 text-white transition-all group-hover:bg-[#1e293b] group-hover:text-orange-500">
+              <li className="flex cursor-default items-center p-2 text-white transition-all group-hover:bg-[#1e293b] group-hover:text-orange-500">
                 {item.label}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="15"
+                  height="15"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-chevron-down"
+                >
+                  <path d="m6 9 6 6 6-6" />
+                </svg>
               </li>
             </ul>
             {item.submenu && (
