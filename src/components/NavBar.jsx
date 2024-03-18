0b8a1f1856f76d17415f515ef366bc930c827b75
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2024-03-17 22:04:55 -0400

    added mobile menu, update style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,40 +1,75 @@
 import { useState } from "react";
+import { Menu } from "lucide-react";
 import { navItems } from "../constants";
 import NavItem from "./NavItem";
 
 function NavBar() {
-  const [activeButton, setActiveButton] = useState(null);
+  const [dropdown, setDropdown] = useState(null);
 
-  const handleButtonClick = (index) => {
-    setActiveButton(index === activeButton ? null : index);
+  const handleDropdownClick = (index) => {
+    setDropdown(index === dropdown ? null : index);
+  };
+
+  const [menu, setMenu] = useState(false);
+
+  const handleMenuClick = () => {
+    setMenu(!menu);
   };
 
   return (
-    <header className="flex flex-row items-center justify-evenly bg-[#0f172a] py-3">
-      <div className="flex flex-row items-center">
-        <img src="/vite.svg" alt="Vite Logo" className="mx-4" />
-        <a className="cursor-default text-3xl">
-          <span className="text-red-500">F</span>
-          <span className="text-orange-500">T</span>
-          <span className="text-yellow-500">I</span>
-          <span className="text-green-500">R</span>
-          <span className="text-teal-500">-</span>
-          <span className="text-blue-500">S</span>
-          <span className="text-indigo-500">I</span>
-          <span className="text-purple-500">S</span>
-        </a>
+    <header className="bg-[#0f172a] p-2">
+      <div className="flex items-center justify-evenly">
+        {/* logo and title */}
+        <div className="flex items-center">
+          <img src="/vite.svg" alt="Vite Logo" className="mx-4" />
+          <a className="cursor-default text-3xl">
+            <span className="text-red-500">F</span>
+            <span className="text-orange-500">T</span>
+            <span className="text-yellow-500">I</span>
+            <span className="text-green-500">R</span>
+            <span className="text-teal-500">-</span>
+            <span className="text-blue-500">S</span>
+            <span className="text-indigo-500">I</span>
+            <span className="text-purple-500">S</span>
+          </a>
+        </div>
+
+        {/* desktop view of nav menus */}
+        <nav className="hidden xl:flex">
+          {navItems.map((item, i) => (
+            <NavItem
+              key={i}
+              item={item}
+              isActive={dropdown === i}
+              onClick={() => handleDropdownClick(i)}
+            />
+          ))}
+        </nav>
+
+        {/* mobile view of nav menus (hamburger) */}
+        <nav className="xl:hidden">
+          <button
+            className={`rounded hover:bg-[#374151] ${menu ? "bg-[#374151]" : ""}`}
+            onClick={handleMenuClick}
+          >
+            <Menu size={35} color="white" />
+          </button>
+        </nav>
       </div>
 
-      <nav className="flex flex-row">
-        {navItems.map((item, i) => (
-          <NavItem
-            key={i}
-            item={item}
-            isActive={activeButton === i}
-            onClick={() => handleButtonClick(i)}
-          />
-        ))}
-      </nav>
+      {/* mobile view of nav menus (menu itself) */}
+      {menu && (
+        <div className="bg-[#0f172a] xl:hidden">
+          {navItems.map((item, i) => (
+            <NavItem
+              key={i}
+              item={item}
+              isActive={dropdown === i}
+              onClick={() => handleDropdownClick(i)}
+            />
+          ))}
+        </div>
+      )}
     </header>
   );
 }
--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,5 +1,6 @@
-import PropTypes from "prop-types";
 import { useState } from "react";
+import { ChevronDown } from "lucide-react";
+import PropTypes from "prop-types";
 
 function NavItem({ item, isActive, onClick }) {
   const [isSubOpen, setSubOpen] = useState(false);
@@ -10,37 +11,27 @@ function NavItem({ item, isActive, onClick }) {
 
   return (
     <div className="relative px-5">
+      {/* menu */}
       <button
-        className={`flex flex-row items-center rounded px-2 py-1  ${isActive ? "bg-[#1e293b] text-orange-500" : "text-white"}`}
+        className={`m-1 flex items-center rounded p-2 hover:bg-[#374151] ${isActive ? "bg-[#1e293b] text-orange-500" : "text-white"} `}
         onClick={() => {
           onClick();
           toggleSubMenu();
         }}
       >
         {item.label}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="15"
-          height="15"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className={`${isActive ? "rotate-180" : ""}`}
-        >
-          <path d="m6 9 6 6 6-6" />
-        </svg>
+        <ChevronDown size={15} className={`${isActive ? "rotate-180" : ""}`} />
       </button>
+
+      {/* submenu */}
       {isActive && (
-        <div className="absolute left-0 mt-2 w-full rounded bg-[#1e293b] shadow-lg">
+        <div className="left-0 mt-1 w-full rounded bg-[#1e293b] p-1 sm:static xl:absolute">
           {item.submenu.map((subItem, index) => (
             <a
               key={index}
               href={subItem.url}
               target="_blank"
-              className="block px-4 py-2 text-white hover:bg-[#374151] hover:text-orange-500"
+              className="m-1 block rounded p-2 text-white hover:bg-[#374151] hover:text-orange-500"
             >
               {subItem.label}
             </a>
