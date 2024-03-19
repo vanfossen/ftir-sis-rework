b30a988ea01b13b76b81e0b195fa8e170459ae22
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2024-03-18 21:38:29 -0400

    update 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
-import NavBar from "../components/NavBar";
 
 function NotFound() {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-800 text-5xl text-white">
-      <NavBar />
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-800 text-white">
+      <img width="150" src="/vite.svg" alt="Vite Logo" />
 
-      <p>404 Not Found</p>
+      <p className="text-9xl">404</p>
 
-      <Link className="bg-gray-700 hover:bg-gray-600" to="/">
-        Home
+      <p className=" text-4xl">Page Not Found</p>
+
+      <Link
+        className="mt-10 rounded bg-gray-700 p-3 text-3xl hover:bg-gray-600 hover:text-orange-500"
+        to="/"
+      >
+        Return To Homepage
       </Link>
     </div>
   );
