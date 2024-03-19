abe073db5c7889ef767330808c92b0e9e567ca00
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2024-03-18 21:39:03 -0400

    add links to logo and text

--- src/components/NavTitle.jsx
+++ src/components/NavTitle.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+
 function NavTitle() {
   return (
     <div className="flex items-center">
-      <img src="/vite.svg" alt="Vite Logo" className="mx-4" />
-      <a className="cursor-default text-3xl">
+      <a
+        href="https://www.rastonlab.org"
+        target="_blank"
+        className="mx-4 rounded p-1 hover:bg-gray-600"
+      >
+        <img src="/vite.svg" alt="Vite Logo" />
+      </a>
+
+      <Link to="/" className="rounded p-1 text-3xl hover:bg-[#374151]">
         <span className="text-red-500">F</span>
         <span className="text-orange-500">T</span>
         <span className="text-yellow-500">I</span>
@@ -11,7 +20,7 @@ function NavTitle() {
         <span className="text-blue-500">S</span>
         <span className="text-indigo-500">I</span>
         <span className="text-purple-500">S</span>
-      </a>
+      </Link>
     </div>
   );
 }
