28a314143f62f8ebd46caf04b5b47186ea755f4c
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2024-03-22 19:31:01 -0400

    close submenu after link click

--- src/components/nav/NavItem.jsx
+++ src/components/nav/NavItem.jsx
@@ -10,6 +10,10 @@ function NavItem({ item, isActive, onClick }) {
     setSubOpen(!isSubOpen);
   };
 
+  const closeSubMenu = () => {
+    setSubOpen(false);
+  };
+
   return (
     <div className="relative px-5">
       {/* menu */}
@@ -32,6 +36,10 @@ function NavItem({ item, isActive, onClick }) {
               key={index}
               to={subItem.route ? subItem.route : subItem.url}
               target={subItem.url ? "_blank" : ""}
+              onClick={() => {
+                onClick();
+                closeSubMenu();
+              }}
               className="m-1 block rounded p-2 text-start text-white hover:bg-[#374151] hover:text-orange-500"
             >
               {subItem.label}
