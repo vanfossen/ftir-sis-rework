a447f7b61a75a5e54f1be6205f580013031d9ac6
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2024-03-24 15:42:38 -0400

    add TextInput component

---
+++ src/components/fields/TextInput.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+
+// https://tailwindcss.com/docs/hover-focus-and-other-states
+function TextInput({ label, type }) {
+  return (
+    <label className="">
+      <span className="text-md block focus:text-orange-400">{label}</span>
+      <input
+        type={type}
+        placeholder="Enter here..."
+        className="mt-1 w-full rounded-md border border-slate-300 bg-white px-3 py-2 text-base placeholder-slate-400 shadow-sm invalid:border-red-600 invalid:text-red-600 focus:border-orange-400 focus:outline-none focus:ring-1 focus:ring-orange-400 focus:invalid:border-red-600 focus:invalid:ring-pink-500 disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none"
+      />
+    </label>
+  );
+}
+
+// PropTypes declaration
+TextInput.propTypes = {
+  label: PropTypes.string,
+  type: PropTypes.string,
+};
+
+export default TextInput;
