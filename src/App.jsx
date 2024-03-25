97cd562ac17a8c0170222d734263dddbdf6d6b7f
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2024-03-24 22:30:11 -0400

    start landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import LandingPage from "./components/LandingPage.jsx";
 import NavBar from "./components/nav/NavBar.jsx";
 
 function App() {
   return (
     <>
       <NavBar />
+      <LandingPage />
       <Outlet />
     </>
   );
