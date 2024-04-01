ddde91a961267a3a50f7215b4384eea37d5dee26
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2024-03-31 20:33:01 -0400

    fix mistake of leftover test code

--- src/components/nav-new/NavBarNew.jsx
+++ src/components/nav-new/NavBarNew.jsx
@@ -1,7 +1,6 @@
 import { ChevronDown, Menu } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import WelcomeDialog from "../WelcomeDialog";
 import CustomAnchor from "./CustomAnchor";
 import CustomLink from "./CustomLink";
 
@@ -64,7 +63,7 @@ const navItems = [
         url: <CustomAnchor url="https://www.example.com/" label="Tutorial" />,
       },
       {
-        url: <WelcomeDialog>Welcome</WelcomeDialog>,
+        url: <CustomAnchor url="https://www.example.com/" label="Welcome" />,
       },
     ],
   },
