717381d93a6298be0a1a1103fc064679dce8420c
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2024-04-04 18:48:10 -0400

    rework text input

--- src/components/fields/TextInput.jsx
+++ src/components/fields/TextInput.jsx
@@ -1,16 +1,21 @@
 import PropTypes from "prop-types";
 
-// https://tailwindcss.com/docs/hover-focus-and-other-states
 function TextInput({ label, type }) {
   return (
-    <label>
-      <span className="text-md block">{label}</span>
+    <div className="relative m-1">
       <input
         type={type}
-        placeholder="Enter here..."
-        className="mt-1 w-full rounded-md border border-slate-300 bg-white px-3 py-2 text-base placeholder-slate-400 shadow-sm invalid:border-red-600 invalid:text-red-600 focus:border-orange-400 focus:outline-none focus:ring-1 focus:ring-orange-400 focus:invalid:border-red-600 focus:invalid:ring-pink-500"
+        id="floating_outlined"
+        className="peer block rounded-lg border px-2.5 pb-2.5 pt-4 text-base shadow-sm focus:border-blue-600 focus:outline-none focus:ring-0"
+        placeholder=""
       />
-    </label>
+      <label
+        htmlFor="floating_outlined"
+        className="absolute start-1 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform cursor-text select-none bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600"
+      >
+        {label}
+      </label>
+    </div>
   );
 }
 
