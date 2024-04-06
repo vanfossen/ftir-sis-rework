798c728abb98cb20ba44ff7250561b2632f8a7bc
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2024-04-05 23:56:05 -0400

    further update toggle

--- src/components/fields/ToggleInput.jsx
+++ src/components/fields/ToggleInput.jsx
@@ -9,20 +9,22 @@ const ToggleInput = ({ label, leftOption, rightOption }) => {
   };
 
   return (
-    <label className="m-1 flex select-none">
-      <span className="underline">{label}</span>
+    <div className="peer select-none items-center justify-center">
+      <span className="text-gray-500">{label}</span>
       <div className="flex">
-        <span>{leftOption}</span>
-        <input
-          type="checkbox"
-          checked={isChecked}
-          onChange={handleCheckboxChange}
-          className="sr-only"
-        />
-        <div className="peer relative mx-3 h-6 w-11 cursor-pointer rounded-full bg-blue-400 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-blue-500 after:transition-all after:content-[''] peer-checked:bg-pink-400 peer-checked:after:translate-x-full peer-checked:after:bg-pink-500 peer-focus:outline-none peer-focus:ring-blue-600"></div>
-        <span>{rightOption}</span>
+        <p>{leftOption}</p>
+        <label className="peer">
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+            className="peer sr-only"
+          />
+          <div className="peer relative mx-3 h-6 w-11 cursor-pointer rounded-full bg-blue-400 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-blue-500 after:transition-all after:content-[''] peer-checked:bg-pink-400 peer-checked:after:translate-x-full peer-checked:after:bg-pink-500 peer-focus:outline-none peer-focus:ring-1 peer-focus:ring-blue-600" />
+        </label>
+        <p>{rightOption}</p>
       </div>
-    </label>
+    </div>
   );
 };
 
